09eae330a04ed5ae2990827c6007972b774b3155
Joshua Ko <khyjs80@gmail.com>
2024-11-11 13:52:37 -0500

    Added CRUD operation for Employee and a form to create or edit employee.

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'employee-table',
@@ -13,12 +14,30 @@ import { CommonModule } from '@angular/common';
 export class EmployeeTableComponent {
   employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService) {}
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.employeeService.getEmployees().subscribe((data: Employee[]) => {
       this.employees = data;
       console.log(data);
     });
+  }
+
+  deleteEmployee(id: number): void {
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: (response) => {
+        this.employees = this.employees.filter((e) => e.id != id);
+      },
+      error: (err) => {
+        console.error('Error deleting employee', err);
+      },
+    });
+  }
+
+  editEmployee(id: number): void {
+    this.router.navigate(['/edit', id]);
   }
 }
--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,5 +1,5 @@
 export interface Employee {
-  id: Number;
+  id: number;
   firstName: string;
   lastName: string;
   phone: string;
